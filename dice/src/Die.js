c461bbc1d8a535170f1baf30911ec699ac728019
goncalofm90 <goncalofm90@gmail.com>
2020-10-14 13:53:02 +0100

    app

---
+++ dice/src/Die.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Die extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Die</h1>
+      </div>
+    )
+  }
+}
+
+export default Die
